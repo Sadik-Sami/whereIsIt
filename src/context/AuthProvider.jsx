32c1b0ff5585766626ea5752cf4b3626ccd48152
Sadik-Sami <miasimanto@gmail.com>
2025-01-01 16:15:02 +0600

    added authcontext and custom hooks for axios and authProvider

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,76 @@
+import { useEffect, useState } from 'react';
+import AuthContext from './AuthContext';
+import {
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+  updateProfile,
+} from 'firebase/auth';
+import app from '../firebase/firebase.init';
+const auth = getAuth(app);
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const googleProvider = new GoogleAuthProvider();
+
+  const createUser = async (email, password) => {
+    setLoading(true);
+    try {
+      const result = await createUserWithEmailAndPassword(auth, email, password);
+      return result;
+    } finally {
+      setLoading(false);
+    }
+  };
+  const updateUserProfile = async (name, photo) => {
+    setLoading(true);
+    try {
+      await updateProfile(auth.currentUser, {
+        displayName: name,
+        photoURL: photo,
+      });
+      setUser((prev) => ({ ...prev, displayName: name, photoURL: photo }));
+    } catch (error) {
+      throw error;
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const signIn = (email, password) => {
+    setLoading(true);
+    return signInWithEmailAndPassword(auth, email, password);
+  };
+  const signInWithGoogle = () => {
+    setLoading(true);
+    return signInWithPopup(auth, googleProvider);
+  };
+  const logOut = () => {
+    return signOut(auth);
+  };
+  useEffect(() => {
+    const unSubscribe = onAuthStateChanged(auth, async (currentUser) => {
+      setUser(currentUser);
+      console.log('State Captured', currentUser);
+    });
+    return () => {
+      unSubscribe();
+    };
+  }, []);
+  const authInfo = {
+    user,
+    loading,
+    createUser,
+    updateUserProfile,
+    signIn,
+    signInWithGoogle,
+    logOut,
+  };
+  return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
+};
+export default AuthProvider;
---
+++ src/hooks/useAxiosSecure.jsx
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+import axios from 'axios';
+import useAuthContext from './useAuthContext';
+import { useNavigate } from 'react-router-dom';
+
+const axiosInstance = axios.create({
+  baseURL: 'http://localhost:5000/',
+  withCredentials: true,
+});
+
+const useAxiosSecure = () => {
+  const { logOut } = useAuthContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axiosInstance.interceptors.response.use(
+      (response) => {
+        return response;
+      },
+      (error) => {
+        console.log('Error Captured in Interceptor', error);
+        if (error.status === 401 || error.status === 403) {
+          logOut()
+            .then(() => navigate('/'))
+            .catch((error) => console.log(error));
+        }
+        return Promise.reject(error);
+      }
+    );
+  }, []);
+  return axiosInstance;
+};
+
+export default useAxiosSecure;
